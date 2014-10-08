06271992dcc9e137266c27c9343b49df2ab7a637
Arthur Zaczek <arthur@dasz.at>
2014-10-08 09:08:57 +0200

    Create a own admin user controller

--- My-MSE-Blog/Controllers/AdminController.cs
+++ My-MSE-Blog/Controllers/AdminController.cs
@@ -8,16 +8,9 @@ namespace MyMSEBlog.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult ListUser()
-        {
-            var bl = new MyMSEBlog.Core.BL.BL(new MyMSEBlog.Core.DAL.FileDAL(Server.MapPath("~/App_Data/Repository.xml")));
-            return View(bl.GetUserList().Skip(0).Take(25));
         }
     }
 }
---
+++ My-MSE-Blog/Controllers/AdminUserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMSEBlog.Controllers
+{
+    public class AdminUserController : Controller
+    {
+        // GET: AdminUser
+        public ActionResult Index()
+        {
+            var bl = new MyMSEBlog.Core.BL.BL(new MyMSEBlog.Core.DAL.FileDAL(Server.MapPath("~/App_Data/Repository.xml")));
+            return View(bl.GetUserList().Skip(0).Take(25));
+        }
+    }
+}
