2c557dfe8689899b3c333bc8fbf21637080419d9
Arthur Zaczek <arthur@dasz.at>
2014-10-09 16:47:33 +0200

    Adding init scripts

---
+++ clone-mse-swe-tempate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "** ERROR: Invalid number of parameter"
+  echo "./clone-mse-swe-tempate.sh MSE-WS??-SWE <Main repo> <Second repo> <Username>"
+  echo ""
+  echo "MSE-WS??-SWE - Example: MSE-WS14-SWE"
+  echo "Main repo    - if-number of your groups main repository. Example: if99x9999"
+  echo "Second repo  - if-number of your groups second repository. Example: if99x0000"
+  echo "Username     - if-number of your user name. Example: if99x9999"
+  echo ""
+  echo "Example"
+  echo "-------"
+  echo "On your PC"
+  echo "./clone-mse-swe-tempate.sh MSE-WS14-SWE if99x9999 if99x0000 if99x9999"
+  echo ""
+  echo "On your colleagues PC:"
+  echo "./clone-mse-swe-tempate.sh MSE-WS14-SWE if99x9999 if99x0000 if99x0000"
+  exit 1;
+fi
+
+if [[ ! $1 =~ MSE-WS[0-9][0-9]-SWE$ ]]; then
+  echo "** ERROR: Parameter 1 is not in a valid format! Must be MSE-WS??-SWE."
+  echo "Example: MSE-WS14-SWE"
+  exit 1;
+fi
+
+repoName=$1
+
+echo "Cloning template"
+echo "================"
+
+git clone https://inf-swe-git.technikum-wien.at/r/MSE/SWE.git "$repoName"
+cd "$repoName"
+chmod +x *.sh
+./setup-remotes.sh $@
---
+++ setup-remotes.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "** ERROR: Invalid number of parameter"
+  echo "./setup-remotes.sh MSE-WS??-SWE <Main repo> <Second repo> <Username>"
+  echo ""
+  echo "MSE-WS??-SWE - Example: MSE-WS14-SWE"
+  echo "Main repo    - if-number of your groups main repository. Example: if99x9999"
+  echo "Second repo  - if-number of your groups second repository. Example: if99x0000"
+  echo "Username     - if-number of your user name. Example: if99x9999"
+  echo ""
+  echo "Example"
+  echo "-------"
+  echo "On your PC"
+  echo "./setup-remotes.sh MSE-WS14-SWE if99x9999 if99x0000 if99x9999"
+  echo ""
+  echo "On your colleagues PC:"
+  echo "./setup-remotes.sh MSE-WS14-SWE if99x9999 if99x0000 if99x0000"
+  exit 1;
+fi
+
+if [[ ! $1 =~ MSE-WS[0-9][0-9]-SWE$ ]]; then
+  echo "** ERROR: Parameter 1 is not in a valid format! Must be MSE-WS??-SWE."
+  echo "Example: MSE-WS14-SWE"
+  exit 1;
+fi
+
+repoName=$1
+mainRepoUserName=$2
+secondaryRepoUserName=$3
+myUserName=$4
+
+echo "Setting up remotes"
+echo "=================="
+
+echo "Repo Name: $repoName"
+echo "Main:      $mainRepoUserName"
+echo "Secondary: $secondaryRepoUserName"
+echo "Username:  $myUserName"
+
+git remote set-url origin "https://$myUserName@inf-swe-git.technikum-wien.at/r/~$mainRepoUserName/$repoName.git"
+
+git remote remove $secondaryRepoUserName
+git remote remove all
+
+git remote add $secondaryRepoUserName https://$myUserName@inf-swe-git.technikum-wien.at/r/~$secondaryRepoUserName/$repoName.git
+git remote add all "https://$myUserName@inf-swe-git.technikum-wien.at/r/~$mainRepoUserName/$repoName.git"
+git remote set-url --add all "https://$myUserName@inf-swe-git.technikum-wien.at/r/~$secondaryRepoUserName/$repoName.git"
+
+echo "Result:"
+git remote -v
