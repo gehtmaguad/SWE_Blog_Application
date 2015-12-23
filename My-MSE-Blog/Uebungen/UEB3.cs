2a8fd5445ecb2beb9ab5c4cf7b6c1193490016b9
Markus Hoesel <markus.hoesel@mobilmail.at>
2015-12-23 14:37:33 +0100

    Implementing UEB3

--- My-MSE-Blog/Uebungen/UEB3.cs
+++ My-MSE-Blog/Uebungen/UEB3.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MSE.SWE.Interfaces;
+using MyMSEBlog.Core.DAL;
+using MyMSEBlog.Core.BL;
+using MyMSEBlog.Core.Interfaces;
 
 namespace MyMSEBlog.Uebungen
 {
@@ -15,7 +19,25 @@ namespace MyMSEBlog.Uebungen
 
         public void SetupContainer(Autofac.ContainerBuilder builder)
         {
-            throw new NotImplementedException();
+            builder.RegisterType<BL>()
+                .As<MyIBL>()
+                .As<IBL>()
+                .InstancePerLifetimeScope();
+
+            // Using physical path as parameter
+            builder.RegisterType<FileDAL>()
+                .As<IDAL>()
+                .WithParameter(new TypedParameter(typeof(string),
+                    "C:/Users/gehtmaguad/Technikum/FH SWE Projekt/My-MSE-Blog/App_Data/Repository.xml"))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<User>()
+                .As<IUser>()
+                .InstancePerDependency();
+
+            builder.RegisterType<BlogPost>()
+                .As<IBlogPost>()
+                .InstancePerDependency();
         }
     }
 }
