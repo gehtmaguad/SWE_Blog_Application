78ec503ca4c390b5fda690db4ea48b772ca1ea3c
Markus Hoesel <markus.hoesel@mobilmail.at>
2016-01-05 17:05:36 +0100

    Refactoring: Using HttpRuntime Cache instead of System.Web.HttpContext.Current.Cache

--- My-MSE-Blog/Controllers/CacheController.cs
+++ My-MSE-Blog/Controllers/CacheController.cs
@@ -17,11 +17,11 @@ namespace MyMSEBlog.Controllers
         {
             lock (typeof(CacheController))
             {
-                IQueryable<IBlogPost> blogPosts = (IQueryable<IBlogPost>)System.Web.HttpContext.Current.Cache["BlogPosts"];
+                IQueryable<IBlogPost> blogPosts = (IQueryable<IBlogPost>)HttpRuntime.Cache["BlogPosts"];
                 if (blogPosts == null)
                 {
                     blogPosts = new BL(new FileDAL("C:/Users/gehtmaguad/Technikum/FH SWE Projekt/My-MSE-Blog/App_Data/Repository.xml")).GetPostList();
-                    System.Web.HttpContext.Current.Cache.Insert("BlogPosts", blogPosts);
+                    HttpRuntime.Cache.Insert("BlogPosts", blogPosts);
                 }
                 return blogPosts;
             }
@@ -30,18 +30,18 @@ namespace MyMSEBlog.Controllers
         public static void UpdateBlogPosts()
         {
             IQueryable<IBlogPost> blogPosts = new BL(new FileDAL("C:/Users/gehtmaguad/Technikum/FH SWE Projekt/My-MSE-Blog/App_Data/Repository.xml")).GetPostList();
-            System.Web.HttpContext.Current.Cache.Insert("BlogPosts", blogPosts);
+            HttpRuntime.Cache.Insert("BlogPosts", blogPosts);
         }
 
         public static IQueryable<IUser> GetUsers()
         {
             lock (typeof(CacheController))
             {
-                IQueryable<IUser> users = (IQueryable<IUser>)System.Web.HttpContext.Current.Cache["Users"];
+                IQueryable<IUser> users = (IQueryable<IUser>)HttpRuntime.Cache["Users"];
                 if (users == null)
                 {
                     users = new BL(new FileDAL("C:/Users/gehtmaguad/Technikum/FH SWE Projekt/My-MSE-Blog/App_Data/Repository.xml")).GetUserList();
-                    System.Web.HttpContext.Current.Cache.Insert("Users", users);
+                    HttpRuntime.Cache.Insert("Users", users);
                 }
                 return users;
             }
@@ -50,7 +50,7 @@ namespace MyMSEBlog.Controllers
         public static void UpdateUsers()
         {
             IQueryable<IUser> users = new BL(new FileDAL("C:/Users/gehtmaguad/Technikum/FH SWE Projekt/My-MSE-Blog/App_Data/Repository.xml")).GetUserList();
-            System.Web.HttpContext.Current.Cache.Insert("Users", users);
+            HttpRuntime.Cache.Insert("Users", users);
         }
 
     }
